eb438b913adbf14d7a4874635e555b8707054422
CJC803 <cjoshcummin@hotmail.com>
2025-12-06 01:18:47 -0500

    Add merge_fighter_profile function to merge profiles

---
+++ utils/fighter_merger.py
@@ -0,0 +1,8 @@
+def merge_fighter_profile(name, tapology, sherdog, ufcstats, odds):
+    return {
+        "name": name,
+        "tapology": tapology,
+        "sherdog": sherdog,
+        "ufcstats": ufcstats,
+        "odds": odds
+    }
